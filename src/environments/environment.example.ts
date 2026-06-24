109a75747323f381408961729aa562113854c914
Juan David Torres Torres <juan.torrestorres@unicolombo.edu.co>
2026-06-24 13:10:43 -0500

    security: remove environment files from tracking, add example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+
+
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://TU_PROJECT_ID.supabase.co',
+  supabaseKey: 'TU_ANON_KEY_AQUI'
+};
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
- supabaseUrl: 'https://cblrwlvlchlpukwqohrr.supabase.co',
-  supabaseKey: 'sb_publishable_sPyD_DkgR00ivyxvyCD0yw_owwnSYGX'
-};
